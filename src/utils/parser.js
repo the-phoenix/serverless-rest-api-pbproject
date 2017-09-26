fada66277e9e6617f94e9e6356fdef3c7359a507
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2017-09-26 20:41:31 +0800

    update event parser

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -2,6 +2,7 @@ export default function parseEvent(event) {
   return {
     data: JSON.parse(event.body || '{}'),
     path: event.requestContext.resourcePath,
+    httpMethod: event.requestContext.httpMethod,
     stage: event.requestContext.stage,
     params: event.pathParameters,
     queryParams: event.queryStringParameters
